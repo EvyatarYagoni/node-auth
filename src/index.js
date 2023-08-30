4d4ea4ab694184e586a48a77fe7b1927ed5f8b1b
Evyatar Yagoni <evyatar@hackeruso.com>
2023-08-30 15:35:44 +0300

    add health route

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 const router = require("./routes");
 const cors = require("cors");
+const {authMiddleware} = require("./middlewares/auth/authMiddleware");
+const healthRouter = require("./routes/health/health");
 require("dotenv").config();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
-app.use('/api', router);
+app.use('/health', healthRouter);
+app.use('/api',authMiddleware, router);
 
 app.listen(port, () => {
     console.log(`app is running on port ${port}`)
---
+++ src/middlewares/auth/authMiddleware.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+
+const authMiddleware = (req, res, next) => {
+    const header = req.headers['authorization'];
+
+    if (!header) {
+        res.status(401).send("Unauthorized");
+    } else {
+        const bearer = header.split(' ');
+        const token = bearer[1];
+        jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+            if (err) {
+                res.status(401).send("Unauthorized");
+            } else {
+                req.decoded = decoded;
+                next();
+            }
+        });
+    }
+}
+
+module.exports = {
+    authMiddleware
+}
