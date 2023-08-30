ad201ae36d8828001f41fb4cfc5282cac6b00785
Evyatar Yagoni <evyatar@hackeruso.com>
2023-08-30 12:33:36 +0300

    create auth router

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+const express = require("express");
+const app = express();
+const router = require("./routes");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(cors());
+app.use('/api', router);
+
+app.listen(port, () => {
+    console.log(`app is running on port ${port}`)
+})
